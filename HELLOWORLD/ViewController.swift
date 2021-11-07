8e2bee809e5493c308dc2ef103a70ed74a6a08c5
Aleksandr Fedotov <sasha4936@yandex.ru>
2021-11-07 12:32:41 +0500

    hide text

--- HELLOWORLD/ViewController.swift
+++ HELLOWORLD/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var toogleButtom: UIButton!
+    
+    @IBOutlet var helloWorldLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloWorldLabel.isHidden = true
+        toogleButtom.layer .cornerRadius = 10
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func makeButtonAction(_ sender: Any) {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            toogleButtom.setTitle("Hide Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            toogleButtom.setTitle("SHOW TEXT", for: .normal)
+        }
+    }
+    
 
 }
 
